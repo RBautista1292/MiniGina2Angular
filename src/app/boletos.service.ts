a2eba54724e0368a082af6abe08f415bae27ebdf
datcougar <werick290@gmail.com>
2023-05-06 18:37:16 -0600

    Que tal

--- src/app/boletos.service.ts
+++ src/app/boletos.service.ts
@@ -1,3 +1,4 @@
+import { Time } from '@angular/common';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class BoletosService {
 
+  private boletos:Boleto[] = [
+  ];
+
   constructor() { }
+
+  getTickets(): Boleto[] {
+    return this.boletos;
+  }
+}
+
+export interface Boleto {
+  usuario: string;
+  pelicula: string;
+  fecha: Date;
+  hora: Time;
+  sala: number;
 }
--- src/app/compra/compra.component.ts
+++ src/app/compra/compra.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pelicula, PeliculasService } from '../peliculas.service';
+import { Boleto, BoletosService } from '../boletos.service';
 
 @Component({
   selector: 'app-compra',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./compra.component.css']
 })
 export class CompraComponent {
-
+  peliculas: Pelicula[];
+  boletos: Boleto[];
+  
+  constructor(public servicio1: PeliculasService, public servicio2: BoletosService) {
+    this.peliculas = this.servicio1.getMovies();
+    this.boletos = this.servicio2.getTickets();
+  }
 }
