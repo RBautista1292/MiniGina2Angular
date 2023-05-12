01f92b51bd8c3f70dff4a63092b38776f57501fe
mauriciocastaeda <117041636+mauriciocastaeda@users.noreply.github.com>
2023-05-12 11:59:20 -0600

    miau :3

--- src/app/citas-registradas/citas-registradas.component.ts
+++ src/app/citas-registradas/citas-registradas.component.ts
@@ -6,11 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./citas-registradas.component.css']
 })
 export class CitasRegistradasComponent implements OnInit {
+  columnas: String[] = [
+    "Nombre", "Correo", "Sala", "Pelicula Seleccionada", "Fecha"
+  ]
+  citas: any[] = [];
+  
+  
+  
   ngOnInit(): void {
     window.scroll({ 
       top: 0, 
       left: 0, 
       behavior: 'smooth' 
     });
+    
+  }
+  constructor(){
+    const valores = localStorage.getItem("formData");
+    if(valores){
+      this.citas = JSON.parse(valores);
+
+
+
+
+
+    }
+    else{
+
+    }
   }
 }
